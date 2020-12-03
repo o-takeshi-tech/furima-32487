aa243977b1bfade279a4d1b7509e838109c3c91d
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-03 17:48:39 +0900

    バリデーションのエラ〜メッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,18 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   
   validates :email, uniqueness: true
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Password include both letters and numbers' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'include both letters and numbers' 
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Last name Full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :last_name
   end
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'First name Full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :first_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Last name kana Full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
     validates :last_name_kana
   end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'First name kana Full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
     validates :first_name_kana
   end
 
