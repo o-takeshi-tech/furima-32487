0ba8e634d941ccd5c93952acc92e5a25cc204449
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-03 18:26:46 +0900

    fakerの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"test"}
-    email                 {"test@example"}
-    password              {"000000"}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name             {}
+    first_name            {}
+    last_name_kana        {}
+    first_name_kana       {}
+    birth_day             {}
+
   end
 end
