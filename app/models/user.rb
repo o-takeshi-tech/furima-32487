1dec4812ab40d8356679c276606dfb904a53180f
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-04 10:37:54 +0900

    Userモデルのバリデーションのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,16 +12,11 @@ class User < ApplicationRecord
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :last_name
-  end
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :first_name
   end
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
     validates :last_name_kana
-  end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
     validates :first_name_kana
   end
-
   validates :birth_day, presence: true
 end
