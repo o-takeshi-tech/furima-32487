67897bb6fff4516baceece5ff55d50cc1055eee3
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-04 17:56:49 +0900

    アソシエーション の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :days_to_deliver
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_fee
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+
+  
 end
