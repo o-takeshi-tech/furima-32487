7f55b72b13e7f6138d72a945b94c6068e9714176
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-07 14:51:55 +0900

    テストコードの記述変更

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,62 +12,68 @@ RSpec.describe Item, type: :model do
       end
     end
     context '出品がうまくいかない場合' do
-      it '画像がないと出品できない' do
+      it 'imageがないと出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it '商品名がないと出品できない' do
+      it 'nameがないと出品できない' do
         @item.name = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
-      it '商品説明がないと出品できない' do
+      it 'descriptionがないと出品できない' do
         @item.description = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it 'カテゴリーが選択されていないと出品できない' do
-        @item.category_id = nil
+      it 'category_idで配列の1番目(---)が選択されていると出品できない' do
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category Select')
       end
-      it '商品状態が選択されていないと出品できない' do
-        @item.status_id = nil
+      it 'status_idで配列の1番目(---)が選択されていると出品できない' do
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Status Select')
       end
-      it '配送料が選択されていないと出品できない' do
-        @item.delivery_fee_id = nil
+      it 'delivery_feeで配列の1番目(---)が選択されていると出品できない' do
+        @item.delivery_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Delivery fee Select')
       end
-      it '発送元地域が選択されていないと出品できない' do
+      it 'prefecture_idで配列の1番目(---)が選択されている出品できない' do
         @item.prefecture_id = nil
+        # include_blank: "--"を使用している為nilを代入
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture Select')
       end
-      it '発送までの日数が選択されていないと出品できない' do
+      it 'dey_to_deliverで配列の1番目(---)が選択されていると出品できない' do
         @item.day_to_deliver = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('Day to deliver Select')
       end
-      it '価格が記載されていないと出品できない' do
+      it 'priceが記載されていないと出品できない' do
         @item.price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '価格の範囲が¥300~¥9,999,999でないと出品できない' do
+      it 'priceが¥10,000,000以上の場合は出品できない' do
+        @item.price = 100000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+      it 'priceが¥299以下の場合は出品できない' do
         @item.price = 100
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
-      it '販売価格が全角だと出品できない' do
-        @item.price = '３３３３３３'
+      it 'priceが全角だと出品できない' do
+        @item.price = "３３３３３３"
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Half-width number')
       end
-      it '販売価格が半角英字だと出品できない' do
+      it 'priceが半角英字だと出品できない' do
         @item.price = 'aaaaaaaa'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Half-width number')
