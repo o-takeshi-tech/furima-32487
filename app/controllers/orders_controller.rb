2bad8388f342b4cb09b636c3b9d4494703db2850
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-09 16:09:18 +0900

    購入手続きのサーバーサイド実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :set_item
   def index
-    @item = Item.find(params[:item_id])
-    @order = Order.new
-  end 
-  
-  def new
+    @order_address = OrderAddress.new
   end 
 
+
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else 
+      render "orders/index"
+    end
   end 
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
