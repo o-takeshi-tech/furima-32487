d8045292104c09f9b9be2e511a74a77908c340a0
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-10 18:57:55 +0900

    ログイン確認の削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,7 +29,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if user_signed_in? && current_user.id == @item.user.id
+    redirect_to root_path if current_user.id == @item.user.id
   end
 
   def pay_item
